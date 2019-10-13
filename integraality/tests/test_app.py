696c1deba05b0cdb4b9ed511fafed9309e9d13e6
Jean-Frédéric <JeanFred@users.noreply.github.com>
2019-10-13 23:50:48 +0200

    Add unit tests for Flask application update endpoint
    The update endpoint is where everything happens,
    so it requires some more advanced testing.
    
    - Separate unit tests in two classes:
      - BasicTests for the simple tests added in 505736f and 6639db1
      - UpdateTests for the new tests for the update endpoint
    - Add test fixtures for the UpdateTests class,
      patching the `PagesProcessor` object
    - Add helper assertion methods, `assertSuccessPage` and `assertErrorPage`
    - Add three unit tests for each use case:
      - Success, resulting in a success page
      - Expected error, resulting in a error page
      - Unexpected error, resulting in a error page with a different message
    
    (We need another test for when no page is provided, but it turns
    out this is currently not well handled by the application.)

--- integraality/tests/test_app.py
+++ integraality/tests/test_app.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8  -*-
 import unittest
+from unittest.mock import patch
 
 from app import app
+from pages_processor import ProcessingException
 
 
 class AppTests(unittest.TestCase):
@@ -9,6 +11,9 @@ class AppTests(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         self.app = app.test_client()
+
+
+class BasicTests(AppTests):
 
     def test_index_page(self):
         response = self.app.get('/')
@@ -19,3 +24,50 @@ class AppTests(unittest.TestCase):
         response = self.app.get('/unexisting_page')
         self.assertEqual(response.status_code, 404)
         self.assertIn("This page does not exist.", response.get_data(as_text=True))
+
+
+class UpdateTests(AppTests):
+
+    def setUp(self):
+        super().setUp()
+        patcher = patch('app.PagesProcessor', autospec=True)
+        self.mock_pages_processor = patcher.start()
+        self.addCleanup(patcher.stop)
+        self.page_title = 'Foo'
+        self.linked_page = '<a href="https://wikidata.org/wiki/{page}">{page}</a>'.format(page=self.page_title)
+
+    def assertSuccessPage(self, response, message):
+        """A custom assertion for a success page."""
+        self.assertEqual(response.status_code, 200)
+        contents = response.get_data(as_text=True)
+        self.assertIn("alert-success", contents)
+        self.assertIn(message, contents)
+
+    def assertErrorPage(self, response, message):
+        """A custom assertion for an error page."""
+        self.assertEqual(response.status_code, 200)
+        contents = response.get_data(as_text=True)
+        self.assertIn("alert-danger", contents)
+        self.assertIn(message, contents)
+
+    def test_update_success(self):
+        response = self.app.get('/update?page=%s' % self.page_title)
+        self.mock_pages_processor.assert_called_once_with()
+        self.mock_pages_processor.return_value.process_one_page.assert_called_once_with(page_title=self.page_title)
+        self.assertSuccessPage(response, 'Updated page ')
+
+    def test_update_error_processing_exception(self):
+        self.mock_pages_processor.return_value.process_one_page.side_effect = ProcessingException
+        response = self.app.get('/update?page=%s' % self.page_title)
+        self.mock_pages_processor.assert_called_once_with()
+        self.mock_pages_processor.return_value.process_one_page.assert_called_once_with(page_title=self.page_title)
+        message = '<p>Something went wrong when updating page {page}.</p>'.format(page=self.linked_page)
+        self.assertErrorPage(response, message)
+
+    def test_update_error_unknown_exception(self):
+        self.mock_pages_processor.return_value.process_one_page.side_effect = ValueError
+        response = self.app.get('/update?page=%s' % self.page_title)
+        self.mock_pages_processor.assert_called_once_with()
+        self.mock_pages_processor.return_value.process_one_page.assert_called_once_with(page_title=self.page_title)
+        message = '<p>Something catastrophic happened when processing page {page}.</p>'.format(page=self.linked_page)
+        self.assertErrorPage(response, message)

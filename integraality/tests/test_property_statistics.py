29b19c3420bc2b41bf7b6eb845b797ec260e332c
André Costa <lokal.profil@gmail.com>
2019-11-05 22:15:20 +0100

    Add unit test for get_query_for_items_for_property (#7)
    In preparation of handling no_grouping queries.
    
    Bug: [T237182](https://phabricator.wikimedia.org/T237182)

--- integraality/tests/test_property_statistics.py
+++ integraality/tests/test_property_statistics.py
@@ -204,6 +204,39 @@ class MakeStatsForOneGroupingTest(PropertyStatisticsTest):
         self.assertEqual(result, expected)
 
 
+class GetQueryForItemsForPropertyPositive(PropertyStatisticsTest):
+
+    def test_get_query_for_items_for_property_positive(self):
+        result = self.stats.get_query_for_items_for_property_positive('P21', 'Q3115846')
+        expected = """
+SELECT DISTINCT ?entity ?entityLabel ?value ?valueLabel WHERE {
+  ?entity wdt:P31 wd:Q41960 .
+  ?entity wdt:P551 wd:Q3115846 .
+  ?entity p:P21 ?prop . OPTIONAL { ?prop ps:P21 ?value }
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+}
+"""
+        self.assertEqual(result, expected)
+
+
+class GetQueryForItemsForPropertyNegative(PropertyStatisticsTest):
+
+    def test_get_query_for_items_for_property_negative(self):
+        result = self.stats.get_query_for_items_for_property_negative('P21', 'Q3115846')
+        expected = """
+SELECT DISTINCT ?entity ?entityLabel WHERE {
+  ?entity wdt:P31 wd:Q41960 .
+  ?entity wdt:P551 wd:Q3115846 .
+  MINUS {
+    {?entity a wdno:P21 .} UNION
+    {?entity wdt:P21 ?prop .}
+  }
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+}
+"""
+        self.assertEqual(result, expected)
+
+
 class SparqlQueryTest(PropertyStatisticsTest):
 
     def setUp(self):

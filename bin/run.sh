344bb30808bf64db93d289aadd1c461d4d21b2b9
Jean-Frédéric <JeanFred@users.noreply.github.com>
2021-04-08 17:49:22 +0200

    Pass wiki as argument to run Shell script
    Add as parameter to `run.sh` the name of the target wiki,
    to be used for bow in logging messages and in the future
    for statsd metrics.

--- bin/run.sh
+++ bin/run.sh
@@ -3,11 +3,11 @@
 # Script to update all dashboards on a given wiki
 #
 # How to use
-# ./bin/run.sh <arguments>
-# All arguments are passed through to the Python script
+# ./bin/run.sh <wiki> <arguments>
+# All arguments after the first one are passed through to the Python script
 #
 # Example
-# ./bin/run.sh https://meta.wikimedia.org
+# ./bin/run.sh meta https://meta.wikimedia.org
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $CURRENT_DIR/defaults.sh
@@ -17,7 +17,10 @@ cd $SOURCE_PATH || exit
 # Use a virtual environment with our requirements
 source $VIRTUAL_ENV_PATH/bin/activate
 
-echo_time "Starting update."
+WIKI="$1"
+shift
+
+echo_time "Starting update for $WIKI."
 
 start_time="$(date -u +%s)"
 
@@ -26,4 +29,4 @@ python integraality/pages_processor.py "$@"
 end_time="$(date -u +%s)"
 elapsed_time="$((end_time-start_time))"
 
-echo_time "Done with the update! ($(TZ=UTC0 printf '%(%H:%M:%S)T\n' $elapsed_time)s)"
+echo_time "Done with the update for $WIKI! ($(TZ=UTC0 printf '%(%H:%M:%S)T\n' $elapsed_time)s)"

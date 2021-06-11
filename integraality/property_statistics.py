JeanFred/inteGraality

commit d4fa36379409a952d3d3a8bbd77dc41b37f982b1

Do not crash on grouping items without an English label
When `grouping_link` is used, integraality looks up the
English label of each grouping item to build the link
(which is debatable, and under re-consideration at T237276)

However, this fails if the item has no English label (with [1]).

Let's just catch the `KeyError`, and agree to revisit
the whole label idea with T237276.

Also add some info-level logging of the issue.

[1] Stacktrace:
```
  File "./integraality/property_statistics.py", line 681, in <module>
    main()
  File "./integraality/property_statistics.py", line 677, in main
    print(stats.retrieve_and_process_data())
  File "./integraality/property_statistics.py", line 619, in retrieve_and_process_data
    text += self.make_stats_for_one_grouping(grouping, item_count, higher_grouping)
  File "./integraality/property_statistics.py", line 546, in make_stats_for_one_grouping
    label = group_item.labels["en"]
  File "/usr/local/lib/python3.7/site-packages/pywikibot/page/_collections.py", line 46, in __getitem__
    return self._data[key]
KeyError: 'en'
```

Bug: T283287

integraality/property_statistics.py
@@ -544,7 +544,8 @@ SELECT (COUNT(?item) as ?count) WHERE {{
                 group_item = pywikibot.ItemPage(self.repo, grouping)
                 group_item.get()
                 label = group_item.labels["en"]
-            except pywikibot.exceptions.InvalidTitle:
+            except (pywikibot.exceptions.InvalidTitle, KeyError):
+                logging.info(f"Could not retrieve label for {grouping}")
                 label = grouping
             text += f('| [[{self.grouping_link}/{label}|{item_count}]] \n')
         else:

commit 5187a4db59c1c6ef602be9c0a63f65e6df2c58f8

Fix incorrect f-string syntax under Python 3.5
This line was introduced in d4fa3637 using f-strings,
which the Toolforge Python3.5 environment does not support.

See also 2abc519e.

```
logging.info(f"Could not retrieve label for {grouping}")
SyntaxError: invalid syntax
```

integraality/property_statistics.py
@@ -569,7 +569,7 @@ SELECT (COUNT(?item) as ?count) WHERE {{
                 group_item.get()
                 label = group_item.labels["en"]
             except (pywikibot.exceptions.InvalidTitle, KeyError):
-                logging.info(f"Could not retrieve label for {grouping}")
+                logging.info(f("Could not retrieve label for {grouping}"))
                 label = grouping
             text += f('| [[{self.grouping_link}/{label}|{item_count}]] \n')
         else:
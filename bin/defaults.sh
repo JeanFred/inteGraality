8bd793d7046405f4a01055afbfbd88efe9609f56
Jean-Frédéric <JeanFred@users.noreply.github.com>
2022-05-14 16:19:52 +0200

    Switch to new Debian Buster toolforge Grid and Python3.7 everywhere
    The previous Tools Grid, running on Strech, was using Python 3.5.
    
    To use it, we forced the use of a Python3.5
    virtual environment for cron jobs (eac4d2c5).
    
    The previous Tools Grid is now deprecated and
    we need to move the workload to Python 3.7 [1].
    
    We were already using Python3.7 for the webservice
    (since 9d5e608e07), so there is not that much to do.
    
    - Remove the eac4d2c5 workaround,
      and let the cron job use the normal venv.
    
    - Connect to a Buster Bastion host in Ansible hosts file
    
    - Add `-release buster` flag to jsub commands in cron jobs
    
    [1] See https://wikitech.wikimedia.org/wiki/News/Toolforge_Stretch_deprecation
    
    Bug: T257942

--- bin/defaults.sh
+++ bin/defaults.sh
@@ -5,7 +5,7 @@ echo_time() {
 # Paths
 : ${SOURCE_PATH:=$HOME/integraality}
 : ${TOOLFORGE_PATH:=$HOME/www/python/}
-: ${VIRTUAL_ENV_PATH:=$TOOLFORGE_PATH/venv35}
+: ${VIRTUAL_ENV_PATH:=$TOOLFORGE_PATH/venv}
 : ${APP_PATH:=$TOOLFORGE_PATH/src}
 : ${STATSD_HOST:=cloudmetrics1001.eqiad.wmnet}
 : ${STATSD_PORT:=8125}

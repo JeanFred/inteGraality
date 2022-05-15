JeanFred/inteGraality

commit f6dc34b057fa3eaaa7b26acf7ad2a251b8788d7d

Remove ww package as python3.5 workaround for f-string
integraality was coded from the beginning using f-strings,
which were only added to Python 3.6.

However, Toolforge only allowed at the time Python 3.5,
and it was still required for Grid Engine jobs.

We thus added in 2abc519e7 a workaround with the `ww` package [1].

Now that Python 3.5 is out of the picture since 8bd793d70
and the move to the Debian Stretch Grid Engine and Python 3.7
we can safely remove the ww package and all the fake f-string syntax.

[1] https://pypi.org/project/ww/

integraality/property_statistics.py
@@ -9,8 +9,6 @@ import logging
 import re
 from enum import Enum
 
-from ww import f
-
 import pywikibot
 import pywikibot.data.sparql
 
@@ -69,7 +67,7 @@ class PropertyStatistics:
         :return: Tuple of two (ordered) dictionaries.
         """
         if self.higher_grouping:
-            query = f("""
+            query = f"""
 SELECT ?grouping (SAMPLE(?_higher_grouping) as ?higher_grouping) (COUNT(DISTINCT ?entity) as ?count) WHERE {{
   ?entity {self.selector_sparql} .
   ?entity wdt:{self.grouping_property} ?grouping .
@@ -78,9 +76,9 @@ SELECT ?grouping (SAMPLE(?_higher_grouping) as ?higher_grouping) (COUNT(DISTINCT
 HAVING (?count >= {self.grouping_threshold})
 ORDER BY DESC(?count)
 LIMIT 1000
-""")
+"""
         elif self.grouping_type == GroupingType.YEAR:
-            query = f("""
+            query = f"""
 SELECT ?grouping (COUNT(DISTINCT ?entity) as ?count) WHERE {{
   ?entity {self.selector_sparql} .
   ?entity wdt:{self.grouping_property} ?date .
@@ -89,9 +87,9 @@ SELECT ?grouping (COUNT(DISTINCT ?entity) as ?count) WHERE {{
 HAVING (?count >= {self.grouping_threshold})
 ORDER BY DESC(?count)
 LIMIT 1000
-""")
+"""
         else:
-            query = f("""
+            query = f"""
 SELECT ?grouping (COUNT(DISTINCT ?entity) as ?count) WHERE {{
   ?entity {self.selector_sparql} .
   ?entity wdt:{self.grouping_property} ?grouping .
@@ -99,7 +97,7 @@ SELECT ?grouping (COUNT(DISTINCT ?entity) as ?count) WHERE {{
 HAVING (?count >= {self.grouping_threshold})
 ORDER BY DESC(?count)
 LIMIT 1000
-""")
+"""
         grouping_counts = collections.OrderedDict()
 
         grouping_groupings = collections.OrderedDict()
@@ -143,33 +141,33 @@ LIMIT 1000
 
     def get_query_for_items_for_property_positive(self, column, grouping):
         column_key = column.get_key()
-        query = f("""
+        query = f"""
 SELECT DISTINCT ?entity ?entityLabel ?value ?valueLabel WHERE {{
-  ?entity {self.selector_sparql} .""")
+  ?entity {self.selector_sparql} ."""
 
         if grouping == self.GROUP_MAPPING.TOTALS:
             pass
 
         elif grouping == self.GROUP_MAPPING.NO_GROUPING:
-            query += f("""
+            query += f"""
   MINUS {{
     ?entity wdt:{self.grouping_property} [] .
-  }}""")
+  }}"""
 
         elif grouping == self.GROUP_MAPPING.UNKNOWN_VALUE:
-            query += f("""
+            query += f"""
   ?entity wdt:{self.grouping_property} ?grouping.
-  FILTER wikibase:isSomeValue(?grouping).""")
+  FILTER wikibase:isSomeValue(?grouping)."""
 
         elif self.grouping_type == GroupingType.YEAR:
-            query += f("""
+            query += f"""
   ?entity wdt:{self.grouping_property} ?date.
   BIND(YEAR(?date) as ?year).
-  FILTER(?year = {grouping}).""")
+  FILTER(?year = {grouping})."""
 
         else:
-            query += f("""
-  ?entity wdt:{self.grouping_property} wd:{grouping} .""")
+            query += f"""
+  ?entity wdt:{self.grouping_property} wd:{grouping} ."""
 
         query += column.get_filter_for_positive_query()
         query += """}
@@ -178,36 +176,36 @@ SELECT DISTINCT ?entity ?entityLabel ?value ?valueLabel WHERE {{
 
     def get_query_for_items_for_property_negative(self, column, grouping):
         column_key = column.get_key()
-        query = f("""
+        query = f"""
 SELECT DISTINCT ?entity ?entityLabel WHERE {{
-  ?entity {self.selector_sparql} .""")
+  ?entity {self.selector_sparql} ."""
 
         if grouping == self.GROUP_MAPPING.TOTALS:
-            query += f("""
-  MINUS {{""")
+            query += """
+  MINUS {{"""
 
         elif grouping == self.GROUP_MAPPING.NO_GROUPING:
-            query += f("""
+            query += f"""
   MINUS {{
-    {{?entity wdt:{self.grouping_property} [] .}} UNION""")
+    {{?entity wdt:{self.grouping_property} [] .}} UNION"""
 
         elif grouping == self.GROUP_MAPPING.UNKNOWN_VALUE:
-            query += f("""
+            query += f"""
   ?entity wdt:{self.grouping_property} ?grouping.
   FILTER wikibase:isSomeValue(?grouping).
-  MINUS {{""")
+  MINUS {{"""
 
         elif self.grouping_type == GroupingType.YEAR:
-            query += f("""
+            query += f"""
   ?entity wdt:{self.grouping_property} ?date.
   BIND(YEAR(?date) as ?year).
   FILTER(?year = {grouping}).
-  MINUS {{""")
+  MINUS {{"""
 
         else:
-            query += f("""
+            query += f"""
   ?entity wdt:{self.grouping_property} wd:{grouping} .
-  MINUS {{""")
+  MINUS {{"""
 
         query += column.get_filter_for_negative_query()
         query += """}
@@ -215,20 +213,20 @@ SELECT DISTINCT ?entity ?entityLabel WHERE {{
         return query
 
     def get_totals_no_grouping(self):
-        query = f("""
+        query = f"""
 SELECT (COUNT(*) as ?count) WHERE {{
   ?entity {self.selector_sparql}
   MINUS {{ ?entity wdt:{self.grouping_property} _:b28. }}
 }}
-""")
+"""
         return self._get_count_from_sparql(query)
 
     def get_totals(self):
-        query = f("""
+        query = f"""
 SELECT (COUNT(*) as ?count) WHERE {{
   ?entity {self.selector_sparql}
 }}
-""")
+"""
         return self._get_count_from_sparql(query)
 
     @staticmethod
@@ -269,8 +267,8 @@ SELECT (COUNT(*) as ?count) WHERE {{
     def get_header(self):
         text = u'{| class="wikitable sortable"\n'
         colspan = 3 if self.higher_grouping else 2
-        text += f('! colspan="{colspan}" |Top groupings (Minimum {self.grouping_threshold} items)\n')
-        text += f('! colspan="{len(self.columns)}"|Top Properties (used at least {self.property_threshold} times per grouping)\n')  # noqa
+        text += f'! colspan="{colspan}" |Top groupings (Minimum {self.grouping_threshold} items)\n'
+        text += f'! colspan="{len(self.columns)}"|Top Properties (used at least {self.property_threshold} times per grouping)\n'  # noqa
         text += u'|-\n'
 
         if self.higher_grouping:
@@ -288,17 +286,17 @@ SELECT (COUNT(*) as ?count) WHERE {{
             "country": "{{Flag|%s}}" % higher_grouping_value,
         }
         if re.match(r"Q\d+", higher_grouping_value):
-            higher_grouping_text = f('{{{{Q|{higher_grouping_value}}}}}')
+            higher_grouping_text = f'{{{{Q|{higher_grouping_value}}}}}'
         elif re.match(r"http://commons.wikimedia.org/wiki/Special:FilePath/(.*?)$", higher_grouping_value):
             match = re.match(r"http://commons.wikimedia.org/wiki/Special:FilePath/(.*?)$", higher_grouping_value)
             image_name = match.groups()[0]
-            higher_grouping_text = f('[[File:{image_name}|center|100px]]')
+            higher_grouping_text = f'[[File:{image_name}|center|100px]]'
             higher_grouping_value = image_name
         elif self.higher_grouping_type in type_mapping:
             higher_grouping_text = type_mapping.get(self.higher_grouping_type)
         else:
             higher_grouping_text = higher_grouping_value
-        return f('| data-sort-value="{higher_grouping_value}"| {higher_grouping_text}\n')
+        return f'| data-sort-value="{higher_grouping_value}"| {higher_grouping_text}\n'
 
     def make_stats_for_no_group(self):
         """
@@ -311,12 +309,12 @@ SELECT (COUNT(*) as ?count) WHERE {{
 
         total_no_count = self.get_totals_no_grouping()
         text += u'| No grouping \n'
-        text += f('| {total_no_count} \n')
+        text += f'| {total_no_count} \n'
 
         for column_entry in self.columns.values():
             column_count = self._get_count_from_sparql(column_entry.get_info_no_grouping_query(self))
             percentage = self._get_percentage(column_count, total_no_count)
-            text += f('| {{{{{self.cell_template}|{percentage}|{column_count}|column={column_entry.get_title()}|grouping={self.GROUP_MAPPING.NO_GROUPING.value}}}}}\n')  # noqa
+            text += f'| {{{{{self.cell_template}|{percentage}|{column_count}|column={column_entry.get_title()}|grouping={self.GROUP_MAPPING.NO_GROUPING.value}}}}}\n'  # noqa
 
         return text
 
@@ -345,11 +343,11 @@ SELECT (COUNT(*) as ?count) WHERE {{
                 group_item.get()
                 label = group_item.labels["en"]
             except (pywikibot.exceptions.InvalidTitle, KeyError):
-                logging.info(f("Could not retrieve label for {grouping}"))
+                logging.info(f"Could not retrieve label for {grouping}")
                 label = grouping
-            text += f('| [[{self.grouping_link}/{label}|{item_count}]] \n')
+            text += f'| [[{self.grouping_link}/{label}|{item_count}]] \n'
         else:
-            text += f('| {item_count} \n')
+            text += f'| {item_count} \n'
 
         for (column_entry_key, column_entry) in self.columns.items():
             try:
@@ -359,7 +357,7 @@ SELECT (COUNT(*) as ?count) WHERE {{
             if not column_count:
                 column_count = 0
             percentage = self._get_percentage(column_count, item_count)
-            text += f('| {{{{{self.cell_template}|{percentage}|{column_count}|column={column_entry.get_title()}|grouping={grouping}}}}}\n')  # noqa
+            text += f'| {{{{{self.cell_template}|{percentage}|{column_count}|column={column_entry.get_title()}|grouping={grouping}}}}}\n'  # noqa
         return text
 
     def make_footer(self):
@@ -368,11 +366,11 @@ SELECT (COUNT(*) as ?count) WHERE {{
         if self.higher_grouping:
             text += u"|\n|"
 
-        text += f('\'\'\'Totals\'\'\' <small>(all items)</small>:\n| {total_items}\n')
+        text += f'\'\'\'Totals\'\'\' <small>(all items)</small>:\n| {total_items}\n'
         for column_entry in self.columns.values():
             totalprop = self._get_count_from_sparql(column_entry.get_totals_query(self))
             percentage = self._get_percentage(totalprop, total_items)
-            text += f('| {{{{{self.cell_template}|{percentage}|{totalprop}|column={column_entry.get_title()}}}}}\n')
+            text += f'| {{{{{self.cell_template}|{percentage}|{totalprop}|column={column_entry.get_title()}}}}}\n'
         text += u'|}\n'
         return text
 
@@ -386,10 +384,10 @@ SELECT (COUNT(*) as ?count) WHERE {{
         try:
             (groupings_counts, groupings_groupings) = self.get_grouping_information()
         except QueryException as e:
-            logging.error(f('No groupings found.'))
+            logging.error('No groupings found.')
             raise e
 
-        logging.info(f('Grouping retrieved: {len(groupings_counts)}'))
+        logging.info(f'Grouping retrieved: {len(groupings_counts)}')
         for (column_entry_key, column_entry) in self.columns.items():
             self.column_data[column_entry_key] = self._get_grouping_counts_from_sparql(column_entry.get_info_query(self))
 

Pipfile
@@ -13,7 +13,6 @@ fakeredis = "*"
 
 [packages]
 pywikibot = "*"
-ww = "*"
 flask = "*"
 redis = "*"
 mwparserfromhell = "*"

integraality/column.py
@@ -6,8 +6,6 @@ Column configuration classes
 
 from enum import Enum
 
-from ww import f
-
 
 class GroupingType(Enum):
     YEAR = "year"
@@ -46,20 +44,20 @@ class AbstractColumn:
 
         :return: (str) SPARQL query
         """
-        query = f("""
+        query = f"""
 SELECT ?grouping (COUNT(DISTINCT ?entity) as ?count) WHERE {{
-  ?entity {property_statistics.selector_sparql} .""")
+  ?entity {property_statistics.selector_sparql} ."""
 
         if property_statistics.grouping_type == GroupingType.YEAR:
-            query += f("""
+            query += f"""
   ?entity wdt:{property_statistics.grouping_property} ?date .
-  BIND(YEAR(?date) as ?grouping).""")
+  BIND(YEAR(?date) as ?grouping)."""
 
         else:
-            query += f("""
-  ?entity wdt:{property_statistics.grouping_property} ?grouping .""")
+            query += f"""
+  ?entity wdt:{property_statistics.grouping_property} ?grouping ."""
 
-        query += f("""
+        query += f"""
   FILTER(EXISTS {{{self.get_filter_for_info()}
   }})
 }}
@@ -67,7 +65,7 @@ GROUP BY ?grouping
 HAVING (?count >= {property_statistics.property_threshold})
 ORDER BY DESC(?count)
 LIMIT 1000
-""")
+"""
         return query
 
     def get_totals_query(self, property_statistics):
@@ -75,13 +73,13 @@ LIMIT 1000
         Get the totals of entities with the column set.
         :return: (str) SPARQL query
         """
-        query = f("""
+        query = f"""
 SELECT (COUNT(*) as ?count) WHERE {{
   ?entity {property_statistics.selector_sparql}
   FILTER(EXISTS {{{self.get_filter_for_info()}
   }})
 }}
-""")
+"""
         return query
 
     def get_info_no_grouping_query(self, property_statistics):
@@ -90,14 +88,14 @@ SELECT (COUNT(*) as ?count) WHERE {{
 
         :return: (str) SPARQL query
         """
-        query = f("""
+        query = f"""
 SELECT (COUNT(*) AS ?count) WHERE {{
   ?entity {property_statistics.selector_sparql} .
   MINUS {{ ?entity wdt:{property_statistics.grouping_property} _:b28. }}
   FILTER(EXISTS {{{self.get_filter_for_info()}
   }})
 }}
-""")
+"""
         return query
 
 
@@ -130,33 +128,33 @@ class PropertyColumn(AbstractColumn):
             property_link = self.property
 
         if self.title:
-            label = f('[[Property:{property_link}|{self.title}]]')
+            label = f'[[Property:{property_link}|{self.title}]]'
         else:
-            label = f('{{{{Property|{property_link}}}}}')
-        return f('! data-sort-type="number"|{label}\n')
+            label = f'{{{{Property|{property_link}}}}}'
+        return f'! data-sort-type="number"|{label}\n'
 
     def get_filter_for_info(self):
         if self.qualifier:
             property_value = f'wd:{self.value}' if self.value else '[]'
-            return f("""
-    ?entity p:{self.property} [ ps:{self.property} {property_value} ; pq:{self.qualifier} [] ]""")
+            return f"""
+    ?entity p:{self.property} [ ps:{self.property} {property_value} ; pq:{self.qualifier} [] ]"""
         else:
-            return f("""
-    ?entity p:{self.property}[]""")
+            return f"""
+    ?entity p:{self.property}[]"""
 
     def get_filter_for_positive_query(self):
-        return f("""
+        return f"""
   ?entity p:{self.property} ?prop . OPTIONAL {{ ?prop ps:{self.property} ?value }}
   SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }}
-""")
+"""
 
     def get_filter_for_negative_query(self):
-        return f("""
+        return f"""
     {{?entity a wdno:{self.property} .}} UNION
     {{?entity wdt:{self.property} ?prop .}}
   }}
   SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }}
-""")
+"""
 
 
 class TextColumn(AbstractColumn):
@@ -176,32 +174,32 @@ class TextColumn(AbstractColumn):
 
     def make_column_header(self):
         if self.title:
-            text = f('{self.title}')
+            text = f'{self.title}'
         else:
-            text = f('{{{{#language:{self.language}}}}}')
-        return f('! data-sort-type="number"|{text}\n')
+            text = f'{{{{#language:{self.language}}}}}'
+        return f'! data-sort-type="number"|{text}\n'
 
     def get_filter_for_info(self):
-        return f("""
+        return f"""
     ?entity {self.get_selector()} ?lang_label.
-    FILTER((LANG(?lang_label)) = '{self.language}').""")
+    FILTER((LANG(?lang_label)) = '{self.language}')."""
 
     def get_filter_for_positive_query(self):
-        return f("""
+        return f"""
   FILTER(EXISTS {{
     ?entity {self.get_selector()} ?lang_label.
     FILTER((LANG(?lang_label)) = "{self.language}").
   }})
   SERVICE wikibase:label {{ bd:serviceParam wikibase:language "{self.language}". }}
-""")
+"""
 
     def get_filter_for_negative_query(self):
-        return f("""
+        return f"""
     {{ ?entity {self.get_selector()} ?lang_label.
     FILTER((LANG(?lang_label)) = "{self.language}") }}
   }}
   SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }}
-""")
+"""
 
 
 class LabelColumn(TextColumn):

integraality/pages_processor.py
@@ -8,7 +8,6 @@ import os
 import re
 
 from redis import StrictRedis
-from ww import f
 
 import pywikibot
 
@@ -140,7 +139,7 @@ class PagesProcessor:
         return properties_data
 
     def replace_in_page(self, output, page_text):
-        regex_text = f('({{{{{self.template_name}.*?(?<!{{{{!)}}}}).*?({{{{{self.end_template_name}}}}})')
+        regex_text = f'({{{{{self.template_name}.*?(?<!{{{{!)}}}}).*?({{{{{self.end_template_name}}}}})'
         regex = re.compile(regex_text, re.MULTILINE | re.DOTALL)
         new_text = re.sub(regex, r'\1\n%s\n\2' % output, page_text, count=1)
         return new_text

commit 80f073ee7cab04edd6996aabe6eae8e35bda9044

Remove unecessary quote to curly brace in `get_query_for_items_for_property_negative`
This was broken in f6dc34b when removing the useless f-string syntax.

integraality/property_statistics.py
@@ -182,7 +182,7 @@ SELECT DISTINCT ?entity ?entityLabel WHERE {{
 
         if grouping == self.GROUP_MAPPING.TOTALS:
             query += """
-  MINUS {{"""
+  MINUS {"""
 
         elif grouping == self.GROUP_MAPPING.NO_GROUPING:
             query += f"""
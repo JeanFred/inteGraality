18b6a343b3107baf38bc660d7b33d6c0c81196f8
Jean-Frédéric <JeanFred@users.noreply.github.com>
2026-05-14 18:04:52 +0200

    Fix test_index_page unit test
    This was broken during the Landing page refresh of 0b33d6ef.

--- integraality/tests/test_app.py
+++ integraality/tests/test_app.py
@@ -18,7 +18,7 @@ class BasicTests(AppTests):
     def test_index_page(self):
         response = self.app.get("/")
         self.assertEqual(response.status_code, 200)
-        self.assertIn("<h1>InteGraality</h1>", response.get_data(as_text=True))
+        self.assertIn("<h1>inteGraality</h1>", response.get_data(as_text=True))
 
     def test_healthz(self):
         response = self.app.get("/healthz")
